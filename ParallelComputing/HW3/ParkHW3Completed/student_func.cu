5e578778230cdb4b84f7c49e60c11ac4dfffc017
Chris <chris.km.park@gmail.com>
2014-09-26 16:22:21 -0400

    Revert "ParallelComputing HW3 student_func.cu completed"
    This reverts commit 7a3ec352c08daa0364b7063994e3ca2da1743039.
    
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/CMakeLists.txt
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/HW3.cu
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/Makefile
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/compare.cpp
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/compare.h
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/loadSaveImage.cpp
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/loadSaveImage.h
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/main.cpp
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial.exr
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial_large.exr
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial_png.gold
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial_png_large.gold
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial_raw.png
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/memorial_raw_large.png
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/reference_calc.cpp
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/reference_calc.h
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/timer.h
    	deleted:    ParallelComputing/HW3/ParkHW3Completed/utils.h

--- ParallelComputing/HW3/ParkHW3Completed/student_func.cu
+++ ParallelComputing/HW3/ParkHW3Completed/student_func.cu
@@ -80,220 +80,48 @@
 */
 
 #include "utils.h"
+using namespace std;
 
-__global__ void reduce_minimum(float * d_out, const float * const d_in, const size_t numItem) {
-    // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
-  extern __shared__ float sdata[];
+float *d_minWorking;
 
-  int myId = threadIdx.x + blockDim.x * blockIdx.x;
-  int tid  = threadIdx.x;
+// Reduce to get the min value
+__global__
+void minReduce(float* const d_values,
+				const size_t numCells)
+{
+	//
+	int x = threadIdx.x;
+	int s = 1;
+	
+	for (int numLeft = numCells; numLeft > 1; s*=2)
+	{
+		if (x % s == 0 && x + s < numCells)
+		{
+			d_values[x] = min(d_values[x], d_values[x + s]);
+		}
+		if (numLeft % 2 == 0)
+		{
+			numLeft /= 2;
+		}
+		else
+		{
+			numLeft = (numLeft + 1)/2;
+		}
 
-  // load shared mem from global mem
-  sdata[tid] = 99999999999.0f;
-  if (myId < numItem)
-    sdata[tid] = d_in[myId];
+		// wait for all threads to finish adding
+		__syncthreads();
+	}
 
-  __syncthreads();            // make sure entire block is loaded!
-
-  // do reduction in shared mem
-  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-    if (tid < s) {
-        sdata[tid] = min(sdata[tid], sdata[tid + s]);
-    }
-    __syncthreads();        // make sure all adds at one stage are done!
-  }
-
-  // only thread 0 writes result for this block back to global mem
-  if (tid == 0) {
-    d_out[blockIdx.x] = sdata[0];
-  }
-}
-
-__global__ void reduce_maximum(float * d_out, const float * const d_in, const size_t numItem) {
-  // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
-  extern __shared__ float sdata[];
-
-  int myId = threadIdx.x + blockDim.x * blockIdx.x;
-  int tid  = threadIdx.x;
-
-  // load shared mem from global mem
-  sdata[tid] = -99999999999.0f;
-  if (myId < numItem)
-    sdata[tid] = d_in[myId];
-
-  __syncthreads();            // make sure entire block is loaded!
-
-  // do reduction in shared mem
-  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-    if (tid < s) {
-      sdata[tid] = max(sdata[tid], sdata[tid + s]);
-    }
-    __syncthreads();        // make sure all adds at one stage are done!
-  }
-
-  // only thread 0 writes result for this block back to global mem
-  if (tid == 0) {
-    d_out[blockIdx.x] = sdata[0];
-  }
-}
-
-__global__ void histogram(unsigned int *d_bins, const float * const d_in, const size_t numBins, const float min_logLum, const float range, const size_t numRows, const size_t numCols) {
-  
-  int myId = threadIdx.x + blockDim.x * blockIdx.x;
-  if (myId >= (numRows * numCols))
-    return;
-
-  float myItem = d_in[myId];
-  int myBin = (myItem - min_logLum) / range * numBins;
-  atomicAdd(&(d_bins[myBin]), 1);
-}
-
-__global__ void scan(unsigned int *d_out, unsigned int *d_sums, const unsigned int * const d_in, const unsigned int numBins, const unsigned int numElems)  {
-
-  extern __shared__ float sdata[];
-  int myId = blockIdx.x * blockDim.x + threadIdx.x;
-  int tid = threadIdx.x;
-  int offset = 1;
-
-  // load two items per thread into shared memory
-  if ((2 * myId) < numBins) {
-    sdata[2 * tid] = d_in[2 * myId];
-  }
-  else {
-    sdata[2 * tid] = 0;
-  }
-  
-  if ((2 * myId + 1) < numBins) {
-    sdata[2 * tid + 1] = d_in[2 * myId + 1];
-  }
-  else {
-    sdata[2 * tid + 1] = 0;
-  }
-
- 	// Reduce
-  for (unsigned int d = numElems >> 1; d > 0; d >>= 1) {
-    if (tid < d)  {
-      int ai = offset * (2 * tid + 1) - 1;
-      int bi = offset * (2 * tid + 2) - 1;
-      sdata[bi] += sdata[ai];
-    }
-    offset *= 2;
-    __syncthreads();
-  }
-    
-  // clear the last element
-  if (tid == 0) {
-    d_sums[blockIdx.x] = sdata[numElems - 1];
-    sdata[numElems - 1] = 0;
-  }
-  
-  // Down Sweep
-  for (unsigned int d = 1; d < numElems; d *= 2) {
-    offset >>= 1;
-    if (tid < d) {
-      int ai = offset * (2 * tid + 1) - 1;
-      int bi = offset * (2 * tid + 2) - 1;
- 	    float t = sdata[ai];
-      sdata[ai] = sdata[bi];
-      sdata[bi] += t;
-    }
-    __syncthreads();
-  }
- 
-  // write the output to global memory
-  if ((2 * myId) < numBins) {
-    d_out[2 * myId] = sdata[2 * tid];
-  }
-  if ((2 * myId + 1) < numBins) {
-    d_out[2 * myId + 1] = sdata[2 * tid + 1];
-  }
-}
-
-// This version only works for one single block! The size of the array of items
-__global__ void scan2(unsigned int *d_out, const unsigned int * const d_in, const unsigned int numBins, const unsigned int numElems)  {
-
-  extern __shared__ float sdata[];
-  int tid = threadIdx.x;
-  int offset = 1;
-
-  // load two items per thread into shared memory
-  if ((2 * tid) < numBins) {
-    sdata[2 * tid] = d_in[2 * tid];  
-  }
-  else {
-    sdata[2 * tid] = 0;
-  }
-
-  if ((2 * tid + 1) < numBins) {
-    sdata[2 * tid + 1] = d_in[2 * tid + 1];  
-  }
-  else {
-    sdata[2 * tid + 1] = 0;
-  }
-
- 	// Reduce
-  for (unsigned int d = numElems >> 1; d > 0; d >>= 1) {
-    if (tid < d)  {
-      int ai = offset * (2 * tid + 1) - 1;
-      int bi = offset * (2 * tid + 2) - 1;
-      sdata[bi] += sdata[ai];
-    }
-    offset *= 2;
-    __syncthreads();
-  }
-    
-  // clear the last element
-  if (tid == 0) {
-    sdata[numElems - 1] = 0;
-  }
-  
-  // Down Sweep
-  for (unsigned int d = 1; d < numElems; d *= 2) {
-    offset >>= 1;
-    if (tid < d) {
-      int ai = offset * (2 * tid + 1) - 1;
-      int bi = offset * (2 * tid + 2) - 1;
- 	    float t = sdata[ai];
-      sdata[ai] = sdata[bi];
-      sdata[bi] += t;
-    }
-    __syncthreads();
-  }
- 
-  // write the output to global memory
-  if ((2 * tid) < numBins) {
-    d_out[2 * tid] = sdata[2 * tid];
-  }
-
-  if ((2 * tid + 1) < numBins) {
-    d_out[2 * tid + 1] = sdata[2 * tid + 1];
-  }
-}
-
-__global__ void add_scan(unsigned int *d_out, const unsigned int * const d_in, const unsigned int numBins) {
-
-  if (blockIdx.x == 0)
-    return;
-
-  int myId = blockIdx.x * blockDim.x + threadIdx.x;
-  unsigned int myOffset = d_in[blockIdx.x];
-
-  if ((2 * myId) < numBins) {
-    d_out[2 * myId] += myOffset;
-  }
-  if ((2 * myId + 1) < numBins) {
-    d_out[2 * myId + 1] += myOffset;
-  }
-
+	// result should be in d_values[0];
 }
 
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
-                                  unsigned int* const d_cdf,
-                                  float &min_logLum,
-                                  float &max_logLum,
-                                  const size_t numRows,
-                                  const size_t numCols,
-                                  const size_t numBins)
+									unsigned int* const d_cdf,
+									float &min_logLum,
+									float &max_logLum,
+									const size_t numRows,
+									const size_t numCols,
+									const size_t numBins)
 {
   //TODO
   /*Here are the steps you need to implement
@@ -306,88 +134,31 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
 
-  // Initialization
-  unsigned int numItem = numRows * numCols;
-  dim3 blockSize(256, 1, 1);
-  dim3 gridSize(numItem / blockSize.x + 1, 1, 1);
-    
-  float * d_inter_min;
-  float * d_inter_max;
-  unsigned int * d_histogram;
-  unsigned int * d_sums;
-  unsigned int * d_incr;
+	int i = 0;
+	cout << "HELP ME\n";
+	
+	// calculate the pixel coordinate for this thread
+	float value = d_logLuminance[0];
 
-  checkCudaErrors(cudaMalloc(&d_inter_min, sizeof(float) * gridSize.x));
-  checkCudaErrors(cudaMalloc(&d_inter_max, sizeof(float) * gridSize.x));
-  checkCudaErrors(cudaMalloc(&d_histogram, sizeof(unsigned int) * numBins));
-  checkCudaErrors(cudaMemset(d_histogram, 0, sizeof(unsigned int) * numBins));
-     
-  // Step 1: Reduce (min and max). It could be done in one step only!
-  reduce_minimum<<<gridSize, blockSize, sizeof(float) * blockSize.x>>>(d_inter_min, d_logLuminance, numItem);
-  reduce_maximum<<<gridSize, blockSize, sizeof(float) * blockSize.x>>>(d_inter_max, d_logLuminance, numItem);
-  numItem = gridSize.x;
-  gridSize.x = numItem / blockSize.x + 1;
+	// allocate second float array of numRows * numCols
+	// do a min reduction op
+	int curCells = numRows * numCols;
+	
+	checkCudaErrors(cudaMalloc(&d_minWorking, sizeof(float) * curCells));
 
-  while (numItem > 1) {
-    reduce_minimum<<<gridSize, blockSize, sizeof(float) * blockSize.x>>>(d_inter_min, d_inter_min, numItem);
-    reduce_maximum<<<gridSize, blockSize, sizeof(float) * blockSize.x>>>(d_inter_max, d_inter_max, numItem);
-    numItem = gridSize.x;
-    gridSize.x = numItem / blockSize.x + 1;
-  }
-
-  // Step 2: Range
-  checkCudaErrors(cudaMemcpy(&min_logLum, d_inter_min, sizeof(float), cudaMemcpyDeviceToHost));
-  checkCudaErrors(cudaMemcpy(&max_logLum, d_inter_max, sizeof(float), cudaMemcpyDeviceToHost));
-
-  float range = max_logLum - min_logLum;
-
-  // Step 3: Histogram
-  gridSize.x = numRows * numCols / blockSize.x + 1;
-  histogram<<<gridSize, blockSize>>>(d_histogram, d_logLuminance, numBins, min_logLum, range, numRows, numCols);
-
-  // Step 4: Exclusive scan - Blelloch
-  unsigned int numElems = 256;
-  blockSize.x = numElems / 2;
-  gridSize.x = numBins / numElems;
-  if (numBins % numElems != 0)
-    gridSize.x++;
-  checkCudaErrors(cudaMalloc(&d_sums, sizeof(unsigned int) * gridSize.x));
-  checkCudaErrors(cudaMemset(d_sums, 0, sizeof(unsigned int) * gridSize.x));
-
-  // First-level scan to obtain the scanned blocks
-  scan<<<gridSize, blockSize, sizeof(float) * numElems>>>(d_cdf, d_sums, d_histogram, numBins, numElems);
-
-  // Second-level scan to obtain the scanned blocks sums
-  numElems = gridSize.x;
-
-  // Look for the next power of 2 (32 bits)
-  unsigned int nextPow = numElems;
-  nextPow--;
-  nextPow = (nextPow >> 1) | nextPow;
-  nextPow = (nextPow >> 2) | nextPow;
-  nextPow = (nextPow >> 4) | nextPow;
-  nextPow = (nextPow >> 8) | nextPow;
-  nextPow = (nextPow >> 16) | nextPow;
-  nextPow++;
-
-  blockSize.x = nextPow / 2;
-  gridSize.x = 1;
-  checkCudaErrors(cudaMalloc(&d_incr, sizeof(unsigned int) * numElems));
-  checkCudaErrors(cudaMemset(d_incr, 0, sizeof(unsigned int) * numElems));
-  scan2<<<gridSize, blockSize, sizeof(float) * nextPow>>>(d_incr, d_sums, numElems, nextPow);
-
-  // Add scanned block sum i to all values of scanned block i
-  numElems = 256;
-  blockSize.x = numElems / 2;
-  gridSize.x = numBins / numElems;
-  if (numBins % numElems != 0)
-    gridSize.x++;
-  add_scan<<<gridSize, blockSize>>>(d_cdf, d_incr, numBins);
-
-  // Clean memory
-  checkCudaErrors(cudaFree(d_inter_min));
-  checkCudaErrors(cudaFree(d_inter_max));
-  checkCudaErrors(cudaFree(d_histogram));
-  checkCudaErrors(cudaFree(d_sums));
-  checkCudaErrors(cudaFree(d_incr));
+	// create array
+	float testArray[] = {4,3,26,21,25,3,6,15,10,3,5,6,7};
+	int numCells = sizeof(testArray)/sizeof(float);
+	cout << "TestArray cells: " << numCells << endl;
+	
+	// copy array
+	checkCudaErrors(cudaMemcpy(d_minWorking, testArray,
+	sizeof(testArray), cudaMemcpyHostToDevice));
+	
+	// gridSize, blockSize
+	minReduce<<<1, numCells>>>(d_minWorking, numCells);
+	checkCudaErrors(cudaMemcpy(testArray, d_minWorking,
+	sizeof(testArray), cudaMemcpyDeviceToHost));
+	cout << "Output should be 2\n";
+	cout << "Output is " << testArray[0] << endl;
 }
